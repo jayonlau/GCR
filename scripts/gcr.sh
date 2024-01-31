b243d13c798f1f625a1c854189cb859685281727
jayonlau <jayonlau@gmail.com>
2024-01-31 14:19:17 +0800

    Update gcr.sh

--- scripts/gcr.sh
+++ scripts/gcr.sh
@@ -12,5 +12,6 @@ for list in `cat k8s.list|grep -v ^$|grep -v ^#`; do
   rel_list=`echo $list | sed 's/\//./g'`
   docker pull $kube_image_repo/$list
   docker tag $kube_image_repo/$list $DOCKERHUB_USERNAME/$list
+  docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWD
   docker push $DOCKERHUB_USERNAME/$list
 done
